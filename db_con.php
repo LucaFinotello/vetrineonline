ba065e2496fe8ba5d373c36b37a4c26a3dbdec80
lucafinotello <luca.finotello93@gmail.com>
2020-06-19 10:27:56 +0200

    aggiunto database

---
+++ db_con.php
@@ -0,0 +1,9 @@
+<?php
+if ($conn= mysqli_connect("localhost", "root", "", "vetrineonline")) {
+//if ($conn= mysqli_connect("localhost", "root", "", "moto")) {
+
+} else {
+
+    echo mysqli_error();
+}
+?>
