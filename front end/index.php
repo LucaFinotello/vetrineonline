0e710f1e78d09e03c5561348aca5b8fbb6e5c562
lucafinotello <luca.finotello93@gmail.com>
2020-07-02 08:47:45 +0200

    aggiunto include header

--- front end/index.php
+++ front end/index.php
@@ -2,20 +2,8 @@
 session_start();
 include("db_con.php");
 include_once('mysql-fix.php');
+include ('header.html');
 ?>
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <title>Vetrineonline</title>
-    <link rel="stylesheet" href="//apps.bdimg.com/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
-    <script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
 <h1>Prenota il tuo turno</h1>
 <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Cerca per giorno...">
 <?php
@@ -64,25 +52,5 @@ else
 </html>
 
 <script>
-    function myFunction() {
-        // Declare variables
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
-        tr = table.getElementsByTagName("tr");
 
-        // Loop through all table rows, and hide those who don't match the search query
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
 </script>
