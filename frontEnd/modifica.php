c1415db4cc3819fc7139eafe38d9ed66d38e5c03
lucafinotello <luca.finotello93@gmail.com>
2020-07-08 11:49:27 +0200

    visualizzazione tavoli frontEnd

--- frontEnd/modifica.php
+++ frontEnd/modifica.php
@@ -37,7 +37,67 @@ include ('header.html');
             </button>
         </div>
     </form>
-    <?php
+        <?php
+        $giorno = $riga["giorno"];
+        echo("<table>");
+
+        $query = "SELECT * FROM sala where giorno = '$giorno'";
+        $numero_colonne = 1;
+        $results = mysqli_query($conn, $query) or die (mysqli_error());
+        if (mysqli_num_rows($results) != 0) {
+            while ($row = mysqli_fetch_array($results)) {
+                $posto = $row['id'];
+                $numeroTavolo = $row['numeroTavolo'];
+                $nome = $row['nome'];
+                $posti = $row['postiTavolo'];
+                $dispo = $row['disponibilita'];
+                if ($numero_colonne <= 10) {
+                    $numero_colonne++;
+                    if ($dispo == 1) {
+                        echo("<td class='postazioni_libere' >N Tavolo: $numeroTavolo <br> posti $posti 
+                                            <form action='prenotaTavolo.php' method='post'>
+                                                <input name='id' value='$posto' hidden>
+                                                <input type='text' name='nome' required/>
+                                                <input type='checkbox' name='prenota' value='0' required/></input>
+                                                <button type='submit'>Prenota</button>
+                                            </form>
+                                                </td>");
+                    } else {
+                        echo("<td class='postazioni_occupate' >N Tavolo: $numeroTavolo <br> posti $posti 
+                                    <form action='prenotaTavolo.php' method='post'>
+                                                <input name='id' value='$posto' hidden>
+                                                <input class='inputPrenotazione' name='nome' value='$nome' readonly>
+                                            </form>
+                                    </td>");
+                    }
+                }
+                if ($numero_colonne == 10) {
+                    $numero_colonne = 1;
+                    if ($dispo == 1) {
+                        echo("<td class='postazioni_libere' >N Tavolo: $numeroTavolo <br> posti $posti 
+                                        <form action='prenotaTavolo.php' method='post'>
+                                                <input name='id' value='$posto' hidden>
+                                                <input type='checkbox' name='prenota' value='0' required/></input>
+                                                <button type='submit'>Prenota</button>
+                                            </form>
+                                        </td></tr>");
+                    } else {
+                        echo("<td class='postazioni_occupate' > N Tavolo: $numeroTavolo <br> posti $posti 
+                                    <form action='prenotaTavolo.php' method='post'>
+                                                <input name='id' value='$posto'>
+                                                <input type='checkbox' name='prenota' value='0' required/></input>
+                                                <button type='submit'>Libera</button>
+                                            </form>
+                                    </td></tr>");
+                    }
+                }
+            }
+        }
+
+        echo("</table>");
+
+        echo "</div></div>";
+
 					}
 			?>
     </body>
