a71fcff51647eb28773f339d81edd295c59eb800
lucafinotello <luca.finotello93@gmail.com>
2020-07-13 11:07:44 +0200

    creazione pdf da form

--- frontEnd/modifica.php
+++ frontEnd/modifica.php
@@ -29,16 +29,17 @@ include ('menu.html');
                 ?>
                 <form action="test.php" method="POST" >
                     <div class="prenotazione">
-                        Data: <input class="inputBottom" name="giorno" type="text" value="<?php echo date('d/m/Y',$riga["giorno"])?>"><br>
-                        Fascia: <input class="inputBottom" name="fascia" type="text" readonly value="<?php echo $riga["fascia"]?>"><br>
-                        Turno: <input class="inputBottom" name="turno" type="text" readonly value="<?php echo $riga["turnoInizio"]?> - <?php echo $riga["turno"]?>"><br>
-                        Stanza: <input class="inputBottom" name="stanza" type="text" value="<?php echo substr($riga["stanza"], 2)?>"><br><br>
+                        Data: <input class="inputBottom" name="giorno" id='data' type="text" value="<?php echo date('d/m/Y',$riga["giorno"])?>"><br>
+                        Fascia: <input class="inputBottom" name="fascia" id='fascia' type="text" readonly value="<?php echo $riga["fascia"]?>"><br>
+                        Turno: <input class="inputBottom" name="turno" id="turno" type="text" readonly value="<?php echo $riga["turnoInizio"]?> - <?php echo $riga["turno"]?>"><br>
+                        Stanza: <input class="inputBottom" name="stanza" id='stanza' type="text" value="<?php echo substr($riga["stanza"], 2)?>"><br><br>
                         <input class="inputBottom" name="disponibilita" type="text" value="<?php echo $riga["disponibilita"]?>" hidden>
                         <input class="inputBottom" name="id" type="text" value="<?php echo $riga["id"]?>" hidden>
                         <button type="submit" class="click" value="Invia" name="Invio">Prenota</button>
                         <button type="reset" class="click">
                             <a href="index.php" style="color: #ffffff;text-decoration: none;">Annulla</a>
                         </button>
+                        <button onclick ="creaPdf()" type = "button">Crea PDF</button>
                     </div>
                 </form>
                 <?php
